da0b56235acb64d1e40cb0aecbfe69c45668de7d
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-07-04 20:47:58 -0400

    making reset to settings.py

--- stock_system_management/settings.py
+++ stock_system_management/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,5 +127,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
